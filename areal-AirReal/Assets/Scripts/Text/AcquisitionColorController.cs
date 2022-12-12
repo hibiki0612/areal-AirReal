f27f09ddfa05316c12835170a95cdc8a1a0d671b
hibiki0612 <bee-metal44@ezweb.ne.jp>
2022-12-12 19:33:33 +0900

    テキスト入力の実装

--- areal-AirReal/Assets/Scripts/Text/AcquisitionColorController.cs
+++ areal-AirReal/Assets/Scripts/Text/AcquisitionColorController.cs
@@ -1,18 +1,56 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.EventSystems;
+using System.IO;
+using  UnityEngine.UI;
 public class AcquisitionColorController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    public Color color;
+    
+    public Texture tex;
+    public Texture2D texture2D;
+    [SerializeField] private GameObject Plate;
+    [SerializeField] private Image image;
+    public RenderTexture texture;
     void Update()
     {
-        
+
+        if (Input.GetMouseButtonDown(0))
+        {
+#if UNITY_EDITOR
+            if (EventSystem.current.IsPointerOverGameObject()) return;
+#else
+	                    if (EventSystem.current.IsPointerOverGameObject(Input.GetTouch(0).fingerId))return;
+#endif
+            
+            Vector3 touchPos = Input.GetTouch(0).position;
+            
+            
+            Ray ray = Camera.main.ScreenPointToRay(touchPos);
+            RaycastHit hit;
+            
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.transform.tag == "PaintCanvas")
+                {
+                    
+                    tex = hit.collider.gameObject.GetComponent<Renderer>().material.mainTexture;
+                    
+                    texture2D = new Texture2D(tex.width, tex.height,TextureFormat.ARGB32, false);
+                    Graphics.CopyTexture(tex, texture2D);
+                    
+                    
+                    image.material.mainTexture = texture2D;
+                    
+                    var png = texture2D.EncodeToPNG();
+                    File.WriteAllBytes("Assets/Image/paint.png", png);
+                    
+                    color = texture2D.GetPixel((int)touchPos.x, (int)touchPos.y);
+                }
+                
+            }
+
+        }
     }
 }
